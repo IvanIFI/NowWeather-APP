1195705780f9d0e921e55ac80c94e54bc514f1f2
Ivan Ferre Insa <iferrein@emeal.nttdata.com>
2025-12-09 17:58:46 +0100

    - Initial commit: Created the project

---
+++ NowWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NowWeather
+//
+//  Created by Ivan Ferre Insa on 9/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
